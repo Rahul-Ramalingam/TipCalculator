3f0b9a05b81422049ae7be1f6ed1d7086be9da52
Rahul-Ramalingam <rahulram30122000@gmail.com>
2021-01-11 21:13:52 +0530

    tip app

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.tipcalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+        setContentView(binding.root)
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = "Enter amount!!"
+            return
+        }
+        val tipPercent = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.amazing_service -> 0.10
+            R.id.good_service -> 0.05
+            else -> 0.02
+        }
+        var tip = cost * tipPercent
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
+/**
+ * the way in which the finding of the component changes
+// Old way with findViewById()
+val myButton: Button = findViewById(R.id.my_button)
+myButton.text = "A button"
+
+// Better way with view binding
+val myButton: Button = binding.myButton
+myButton.text = "A button"
+
+// Best way with view binding and no extra variable
+binding.myButton.text = "A button
+ */
+
